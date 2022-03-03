152e4baf64dc694c8f9d8668214015a171e2f163
Gideon Razzaq <gideonrazzaq1158@gmail.com>
2022-03-03 01:16:21 -0500

    added screenshot, finished files

---
+++ react-portfolio/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Gideonraz/react-portfolio'
+      className='link footer__link'
+    >
+      Created By Gideon Razzaq
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ react-portfolio/src/portfolio.js
@@ -0,0 +1,66 @@
+const header = {
+ 
+  homepage: 'https://gideonraz.github.io/react-portfolio/',
+  title: 'JS.',
+}
+
+const about = {
+  name: 'Gideon Razzaq',
+  role: 'Full Stack Web Developer',
+  description:
+    'Recent Full Stack Web Development graduate with education in web development. Highly poised and dedicated with talent in design principles and programming languages. Excellent eye for visual aesthetics paired with insight into data and backend functionality. User-minded Web Developer prioritizes usability and adaptability during projects. Designs sites and apps to attract users with aesthetically pleasing interfaces and exceptional UX elements. Comfortable working within large-scale collaborations or on individually- developed assets. Savvy Web Developer eager to integrate rich media and exciting assets to grow usership. Innovates web design through novel site elements and app capabilities. Passionate about presenting users and clients with new, useful, and attractive assets for online consumption.',
+  resume: 'https://drive.google.com/file/d/1oxU3yV78T03bZGs27MKiw0SZTjv5D0AP/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/gideon-razzaq-151296232/',
+    github: 'https://github.com/Gideonraz',
+  },
+}
+
+const projects = [
+  {
+    name: 'Artistify',
+    description:
+      'This application allows users to create or view webpages for music artists. Users can create their own account and give input in order to create these webpages.',
+    stack: ['HTML', 'API', 'Java'],
+    sourceCode: 'https://github.com/mikepizzica/artistify',
+    livePreview: 'https://artistify-project.herokuapp.com/',
+  },
+  {
+    name: 'Many Recipes',
+    description:
+      'The application, will provide visitors with a plotform to share and recipes from across the world that they can make in there own home. Recipes will vary in spice level.',
+    stack: ['HTML', 'Java', 'React'],
+    sourceCode: 'https://github.com/Yassin1/ManyRecipes',
+    livePreview: 'https://stormy-headland-62779.herokuapp.com/',
+  },
+  {
+    name: 'Book Search Engine',
+    description:
+      'This web application lets the user search for books and can save them by creating an account and logging in to view your saved books.',
+    stack: ['HTML', 'Java', 'API'],
+    sourceCode: 'https://github.com/Gideonraz/book-search-engine',
+    livePreview: 'https://gideonraz.github.io/book-search-engine/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'MySQL',
+  'Material UI',
+  'Git',
+  'Databases',
+  'Jest',
+  'CMS platforms',
+]
+
+const contact = {
+  
+  email: 'gideonrazzaq1158@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
